8b10fb197a084a290e0069fbffbbdd790b261913
Darko-beep <darkofrederick98@outlook.com>
2026-05-11 10:12:37 +0000

    advanced node server done

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 // creating a server using express
-
-const express = require('express');
-const morgan = require('morgan');
-
-const app = express();
+const http = require('http'); //require the http module   
+const app = require('./app/app'); //require the app module
 
 //port 
 const PORT = process.env.PORT || 2020;
 
-//middleware using morgan 
-app.use(morgan('dev'));
+
 
 
 //server 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+//listen to the server  
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
